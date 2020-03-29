48bc5e1c7f8df9c264e9c9c67973b1a5ae9df7a0
songwei <26887536@qq.com>
2020-03-30 00:15:33 +0800

    自定义用户失败，setting指定自定义用户模型顺序不对，应该最先指定顺序然后在创建自定义用户表

--- EpManage/models.py
+++ EpManage/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
@@ -44,28 +44,60 @@ class Section(models.Model):
         verbose_name_plural = '科室总表'
 
 
-class EpProfile(models.Model):
-    user = models.OneToOneField(User, verbose_name='员工总表', on_delete=models.CASCADE)
-    department = models.ForeignKey('Department', verbose_name='所属部门', null=True, on_delete=models.SET_NULL)
+class MyUserManage(BaseUserManager):
+
+    def create_user(self, user_id, username, password=None):
+        user = self.model(
+            user_id=user_id,
+            username=username,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, user_id, username, password=None):
+        user = self.create_user(
+            user_id=user_id,
+            password=password,
+            username=username,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+    '''不含权限以及分组管理'''
+    user_id = models.SmallIntegerField(verbose_name='员工编号', unique=True)
+    username = models.CharField(verbose_name='姓名', max_length=64, unique=True)
     section = models.ForeignKey('Section', verbose_name='所属科室', null=True, on_delete=models.SET_NULL)
-    uid = models.SmallIntegerField(unique=True, null=True, verbose_name='员工编号')
-    phone = models.CharField(max_length=64, verbose_name='电话号码')
+    department = models.ForeignKey('Department', verbose_name='所属部门', null=True, on_delete=models.SET_NULL)
+    plat = models.ForeignKey('Plat', verbose_name='所属平台', null=True, on_delete=models.SET_NULL)
+    phone = models.CharField(max_length=64, verbose_name='电话')
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = MyUserManage()
+
+    USERNAME_FIELD = 'user_id'
+    REQUIRED_FIELDS = 'username'
 
     def __str__(self):
-        return '%s %s %s' % (self.user_id, self.user, self.department)
+        return '%s %s %s' % (self.user_id, self.username, self.department)
 
     class Meta:
         verbose_name = '员工总表'
         verbose_name_plural = '员工总表'
 
+    ''' def has_perm(self, perm, obj=None):
+           pass
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        EpProfile.objects.create(user = instance)
+       def has_moudle_perm(self, app_label):
+           pass
 
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+       @property
+       def is_staff(self):
+           pass
+           权限验证'''
 
--- dpmanage/settings.py
+++ dpmanage/settings.py
@@ -127,3 +127,5 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'EpManage.MyUser'
